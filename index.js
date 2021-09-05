a9e2f7d83f69c8900d1009d7b335d312b470f7a0
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-09-05 16:22:43 -0500

    completed task 5

--- index.js
+++ index.js
@@ -198,8 +198,8 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/) {
-  /*add your code here*/
+function miles(km) {
+  return km * 0.621371
 }
 
 //Task 5b - Feet to CM
@@ -210,8 +210,8 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/) {
-  /*add your code here*/
+function feet(cm) {
+  return cm / 30.48
 }
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
